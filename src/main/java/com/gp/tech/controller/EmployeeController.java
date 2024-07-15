29b2e6f860648e1bf8ff8e9e38c408247246ec89
Adityatech16 <gajanangaikawad15@gmail.com>
2024-07-15 13:56:49 +0530

    create update and delete  api

--- src/main/java/com/gp/tech/controller/EmployeeController.java
+++ src/main/java/com/gp/tech/controller/EmployeeController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -42,6 +44,24 @@ public class EmployeeController {
 		List<Employee> allEmployees = empservice.getAllEmployees();
 
 		return new ResponseEntity<>(allEmployees, HttpStatus.OK);
+
+	}
+
+	@PutMapping("update/{id}")
+	public ResponseEntity<Employee> updateEmployee(@PathVariable Integer id, @RequestBody Employee emp) {
+
+		Employee updateEmp = empservice.updateEmp(id, emp);
+
+		return new ResponseEntity<>(updateEmp, HttpStatus.CREATED);
+
+	}
+
+	@DeleteMapping("{id}")
+	public ResponseEntity<String> deleteEmployee(@PathVariable Integer id) {
+
+		String deleteEmployee = empservice.deleteEmployee(id);
+
+		return new ResponseEntity<>("Em[ployee is deleted", HttpStatus.OK);
 
 	}
 
